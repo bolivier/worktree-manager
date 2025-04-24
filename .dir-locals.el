31f49f7c98f10b872786f00b404721ad5d7e1235
Brandon Olivier <brandon@brandonolivier.com>
2025-04-23 21:12:41 -0500

    Delete dir-locals

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables         -*- no-byte-compile: t; -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((clojure-mode
-  . ((clojure-indent-style . :always-indent)
-     (clojure-indent-vars
-      . ((->result . 1))))))
